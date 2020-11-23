fa1ec0e79e5c19a762787bf7866440353dc3a9b5
TangYM <Yuanzhi.Tang@cn.unisys.com>
2020-11-23 21:44:25 +0800

    Add ListView Sample

---
+++ listview/FlagFacts/AllFlagsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using FlagData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FlagFacts
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AllFlagsPage : ContentPage
+	{
+		public AllFlagsPage()
+		{
+			InitializeComponent();
+
+			BindingContext = DependencyService.Get<FlagDetailsViewModel>();
+		}
+
+		async void OnItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			await this.Navigation.PushAsync(new FlagDetailsPage());
+		}
+	}
+}
